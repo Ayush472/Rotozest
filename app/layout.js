cefdc7715c3fc37aba1c1f58f6ef5e0ee5922901
My Name <191200107035@asoit.edu.in>
2024-07-28 20:45:38 +0530

    meta tag for robots and sitemap file create

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Rotary Gear Pump, Rotary Pump, Gear Pump | Rotozest Industries",
-  description: "Rotozest Industries  is a leading Manufacturer and Supplier of Rotary Gear Pump, Gear Pump, Rotary Pump, and Rotary Gear Pumps in India.",
+  description:
+    "Rotozest Industries  is a leading Manufacturer and Supplier of Rotary Gear Pump, Gear Pump, Rotary Pump, and Rotary Gear Pumps in India.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        name="description"
+        content="Rotozest Industries - High-quality rotary gear pumps for industrial, pharmaceutical, and food applications."
+      />
+      <meta
+        name="keywords"
+        content="Rotary gear pump, Stainless steel pump, Self-priming pump, Positive displacement pump, Industrial pumps, SS-316 pump, Viscous liquid pump, Food product pump, Pharmaceutical pump, Chemical pump"
+      />
+      <meta name="author" content="Rotozest Industries" />
+      <meta property="og:type" content="website" />
       <body className={inter.className}>{children}</body>
     </html>
   );
